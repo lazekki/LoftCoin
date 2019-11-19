fccf8a16b2fc9225347d96d1a398df8a4f007a3a
Olga Zaharenko <olgaagladze@gmail.com>
2019-11-19 21:50:05 +0200

    homework_2. 45_02

--- app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/splash/SplashActivity.java
+++ app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/splash/SplashActivity.java
@@ -2,9 +2,12 @@ package com.loftschool.ozaharenko.loftcoin19.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.loftschool.ozaharenko.loftcoin19.R;
+import com.loftschool.ozaharenko.loftcoin19.ui.welcome.WelcomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(()-> {
+            startActivity(new Intent(this, WelcomeActivity.class));
+            finish();
+        }, 1000);
+
     }
 }
--- app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/welcome/WelcomeActivity.java
@@ -5,14 +5,22 @@ import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.loftschool.ozaharenko.loftcoin19.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_welcome);
+        recyclerView = findViewById(R.id.pager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
     }
 }
+
+
