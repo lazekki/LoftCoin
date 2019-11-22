af7fc160e79b2c5ba8214c40a0b912f6c2e93315
lazekki <lazekkian@gmail.com>
2019-11-22 16:25:41 +0200

    Homework_2_Dots

--- app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/loftschool/ozaharenko/loftcoin19/ui/welcome/WelcomeActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,13 +30,23 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         recyclerView = findViewById(R.id.pager);
+        recyclerView.setNestedScrollingEnabled(false); //for dots
         recyclerView.setLayoutManager(new LinearLayoutManager(
                 this, RecyclerView.HORIZONTAL, false));
+        recyclerView.setHasFixedSize(true); //for dots
+
+        recyclerView.swapAdapter(new WelcomePageAdapter(), false);
+
+        final int radius = getResources().getDimensionPixelSize(R.dimen.radius); //for dots
+        final int dotsHeight = getResources().getDimensionPixelSize(R.dimen.dots_height); //for dots
+        final int color = ContextCompat.getColor(this, R.color.primaryTextColor);//for dots
+        final int colorIn = ContextCompat.getColor(this, R.color.secondaryLightColor);//for dots
+
+        recyclerView.addItemDecoration(new DotsIndicatorDecoration(radius, radius * 4, dotsHeight, colorIn, color)); //for dots
 
         snapHelper = new PagerSnapHelper();
         snapHelper.attachToRecyclerView(recyclerView);
 
-        recyclerView.swapAdapter(new WelcomePageAdapter(), false);
         final Settings settings = new Settings(getApplicationContext());
         findViewById(R.id.btn_start).setOnClickListener((view) -> {
             startActivity(new Intent(this, MainActivity.class));
